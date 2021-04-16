e038a648c4706901e478f0f1447139b4ac829196
Brent Robbins <brent@variantstudios.com>
2021-04-16 12:13:50 -0600

    Testing the home page by removing everything.

--- pages/index.js
+++ pages/index.js
@@ -1,31 +1,41 @@
-import { getSiteSettings } from '@/lib/api'
-import Header from '@/components/Header'
-import { Layout } from '@/components/Layout'
-import { Text, Flex, Box } from "@chakra-ui/react"
-import { Question } from '@/components/Question'
-import {Form} from '@/components/Form'
+import React from 'react'
 
-export default function Home({siteSettings}) {
+export default function Home() {
   return (
-    <Layout siteSettings={siteSettings}>
-      <Flex paddingY={'5rem'} flex='1' flexDir='column' justifyContent='center' alignItems='center'>
-        <Header title={siteSettings.title} />
-        <Text>
-         coming soon
-        </Text>
-
-        <Box mt='4rem' width='full'><Question /></Box>
-      </Flex>
-    </Layout>
+    <div>
+      home
+    </div>
   )
 }
-// <Box>
-// <Form _id='feedback' />
-// </Box>
-export async function getStaticProps() {
-  const siteSettings = await getSiteSettings()
-  return {
-    props: { siteSettings },
-    revalidate: 1
-  }
-}
+
+// import { getSiteSettings } from '@/lib/api'
+// import Header from '@/components/Header'
+// import { Layout } from '@/components/Layout'
+// import { Text, Flex, Box } from "@chakra-ui/react"
+// import { Question } from '@/components/Question'
+// import {Form} from '@/components/Form'
+
+// export default function Home({siteSettings}) {
+//   return (
+//     <Layout siteSettings={siteSettings}>
+//       <Flex paddingY={'5rem'} flex='1' flexDir='column' justifyContent='center' alignItems='center'>
+//         <Header title={siteSettings.title} />
+//         <Text>
+//          coming soon
+//         </Text>
+
+//         <Box mt='4rem' width='full'><Question /></Box>
+//       </Flex>
+//     </Layout>
+//   )
+// }
+// // <Box>
+// // <Form _id='feedback' />
+// // </Box>
+// export async function getStaticProps() {
+//   const siteSettings = await getSiteSettings()
+//   return {
+//     props: { siteSettings },
+//     revalidate: 1
+//   }
+// }
