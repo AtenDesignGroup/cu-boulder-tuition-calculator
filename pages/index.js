266a2d459b5554c93e77af5227bc63d2e1b4fbc2
Brent Robbins <brent@variantstudios.com>
2021-04-16 12:29:47 -0600

    Reverted the home page. Setup the netlify local dev

--- pages/index.js
+++ pages/index.js
@@ -1,41 +1,39 @@
-import React from 'react'
+import { getSiteSettings } from "@/lib/api";
+import Header from "@/components/Header";
+import { Layout } from "@/components/Layout";
+import { Text, Flex, Box } from "@chakra-ui/react";
+import { Question } from "@/components/Question";
+import { Form } from "@/components/Form";
 
-export default function Home() {
+export default function Home({ siteSettings }) {
   return (
-    <div>
-      home
-    </div>
-  )
+    <Layout siteSettings={siteSettings}>
+      <Flex
+        paddingY={"5rem"}
+        flex="1"
+        flexDir="column"
+        justifyContent="center"
+        alignItems="center"
+      >
+        <Header title={siteSettings.title} />
+        <Text>coming soon</Text>
+
+        <Box>
+          <Form _id="feedback" />
+        </Box>
+
+        <Box mt="4rem" width="full">
+          <Question />
+        </Box>
+      </Flex>
+    </Layout>
+  );
 }
 
-// import { getSiteSettings } from '@/lib/api'
-// import Header from '@/components/Header'
-// import { Layout } from '@/components/Layout'
-// import { Text, Flex, Box } from "@chakra-ui/react"
-// import { Question } from '@/components/Question'
-// import {Form} from '@/components/Form'
-
-// export default function Home({siteSettings}) {
-//   return (
-//     <Layout siteSettings={siteSettings}>
-//       <Flex paddingY={'5rem'} flex='1' flexDir='column' justifyContent='center' alignItems='center'>
-//         <Header title={siteSettings.title} />
-//         <Text>
-//          coming soon
-//         </Text>
-
-//         <Box mt='4rem' width='full'><Question /></Box>
-//       </Flex>
-//     </Layout>
-//   )
-// }
-// // <Box>
-// // <Form _id='feedback' />
-// // </Box>
-// export async function getStaticProps() {
-//   const siteSettings = await getSiteSettings()
-//   return {
-//     props: { siteSettings },
-//     revalidate: 1
-//   }
-// }
+export async function getStaticProps() {
+  const siteSettings = await getSiteSettings();
+  return {
+    props: { siteSettings },
+    revalidate: 1,
+  };
+}
