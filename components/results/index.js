58064f9858a98da511cb6a0ab15aeb11057c1ffd
Brent Robbins <brent@variantstudios.com>
2021-07-30 05:22:42 -0600

    cubtc-19 Accessibility tweak to the results page.

--- components/results/index.js
+++ components/results/index.js
@@ -16,7 +16,8 @@ import {
   RadioGroup,
   Text,
   Button,
-  Link as ChakraLink
+  FormLabel,
+  Link as ChakraLink,
 } from '@chakra-ui/react'
 import { Text as BodyText } from '@/components/serializers/text'
 import { Counter } from '@/components/counter'
@@ -29,37 +30,37 @@ export function Results({ categories, tuitionCalculator, dev }) {
   const mainRef = useRef()
   const componentRef = useRef()
 
-  const updateTotalSemesters = val => {
+  const updateTotalSemesters = (val) => {
     val = parseInt(val)
     actions.updateAction({
       ...state,
       calculator: {
         ...state.calculator,
         totalSemesters: val,
-        ...state.calculator.results
-      }
+        ...state.calculator.results,
+      },
     })
   }
 
   const filteredResults = categories
-    ?.map(val => {
+    ?.map((val) => {
       return {
         ...val,
         lineItems: val.lineItems
-          .map(item => {
+          .map((item) => {
             return {
               ...item,
-              total: totalGenerator(item?.itemValue[0], questions)
+              total: totalGenerator(item?.itemValue[0], questions),
             }
           })
-          .filter(val => showArray(val, questions) === true)
+          .filter((val) => showArray(val, questions) === true),
       }
     })
-    .filter(val => val.lineItems.length > 0)
-    .map(val => {
+    .filter((val) => val.lineItems.length > 0)
+    .map((val) => {
       const catTotals = []
       let catTotal = 0
-      val.lineItems.map(item => {
+      val.lineItems.map((item) => {
         // console.log(item.frequency)
         catTotals.push(
           item.frequency === 'perSemester'
@@ -72,13 +73,13 @@ export function Results({ categories, tuitionCalculator, dev }) {
       })
       return {
         ...val,
-        total: catTotal
+        total: catTotal,
       }
     })
 
   const grandTotals = []
   let grandTotal = 0
-  filteredResults.map(item => {
+  filteredResults.map((item) => {
     grandTotals.push(parseFloat(item.total))
     grandTotals.length > 0
       ? (grandTotal = grandTotals.reduce((a, b) => parseFloat(a) + parseFloat(b), 0))
@@ -95,10 +96,10 @@ export function Results({ categories, tuitionCalculator, dev }) {
         showResults: false,
         questions: [],
         results: [],
-        totalSemesters: 1
-      }
+        totalSemesters: 1,
+      },
     })
-    if(dev){
+    if (dev) {
       router.push(`/dev`)
     } else {
       router.push(`/`)
@@ -109,18 +110,6 @@ export function Results({ categories, tuitionCalculator, dev }) {
     <Box mb={10} className="printNoMargins">
       <Box ref={componentRef}>
         <Box mb={10} className="printNoMargins">
-          <Heading
-            size="sm"
-            mb={4}
-            color="gray.600"
-            ref={mainRef}
-            // tabIndex="-1"
-            as="h1"
-            className="printNoMargins"
-          >
-            Estimate costs for:
-          </Heading>
-
           <Flex alignItems="center" className="printVisibilityHide" mb={10}>
             <RadioGroup
               name="totalSemestersView"
@@ -129,7 +118,9 @@ export function Results({ categories, tuitionCalculator, dev }) {
               defaultChecked={totalSemesters.toString()}
               as="fieldset"
             >
-              <legend className="visuallyHidden">Choose number of semesters to view</legend>
+              <FormLabel fontWeight="bold" as="legend">
+                Estimate costs for:
+              </FormLabel>
               <Stack direction="row">
                 <Radio value="1" id="one" colorScheme="yellow">
                   One Semester
@@ -149,11 +140,18 @@ export function Results({ categories, tuitionCalculator, dev }) {
             p="30px"
             flexDirection={{ base: 'column', md: 'row' }}
           >
-            <Heading size="lg" as="h2" mb={{ base: '1', md: '0' }}>
+            <Heading size="lg" as="h1" mb={{ base: '1', md: '0' }}>
               Your total estimate
             </Heading>
 
-            <Text fontSize="2xl" mb="0" fontWeight="bold" color="gray.800" className="printPrice" suppressHydrationWarning={true}>
+            <Text
+              fontSize="2xl"
+              mb="0"
+              fontWeight="bold"
+              color="gray.800"
+              className="printPrice"
+              suppressHydrationWarning={true}
+            >
               <Counter target={grandTotal} duration={2} />{' '}
               <ChakraLink href="#disclaimer" aria-label="View disclaimer" textDecoration="none">
                 *
@@ -163,7 +161,7 @@ export function Results({ categories, tuitionCalculator, dev }) {
         </Box>
 
         <Box mb={10} className="printNoMargins" pt={0}>
-          {filteredResults.map(category => (
+          {filteredResults.map((category) => (
             <Category category={category} key={category._id} questions={questions} />
           ))}
         </Box>
@@ -201,14 +199,14 @@ export function Results({ categories, tuitionCalculator, dev }) {
             }}
             content={() => componentRef.current}
           />
-          {(questions && Object.keys(questions).length > 0 && dev) && (
+          {questions && Object.keys(questions).length > 0 && dev && (
             <Button leftIcon={<FaPencilAlt />} variant="link" fontSize="sm" colorScheme="blue">
-            <a href={`/dev/question/${Object.keys(questions)[0]}`}>Edit Answers</a>
+              <a href={`/dev/question/${Object.keys(questions)[0]}`}>Edit Answers</a>
             </Button>
           )}
-          {(questions && Object.keys(questions).length > 0 && !dev) && (
+          {questions && Object.keys(questions).length > 0 && !dev && (
             <Button leftIcon={<FaPencilAlt />} variant="link" fontSize="sm" colorScheme="blue">
-            <a href={`/question/${Object.keys(questions)[0]}`}>Edit Answers</a>
+              <a href={`/question/${Object.keys(questions)[0]}`}>Edit Answers</a>
             </Button>
           )}
         </Flex>
