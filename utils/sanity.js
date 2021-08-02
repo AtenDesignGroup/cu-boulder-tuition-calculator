81b7de2ca80758f83ec29e4756e2d78000b9109b
Brent Robbins <brent@variantstudios.com>
2021-08-02 11:01:09 -0600

    sanity cdn settings updated.

--- utils/sanity.js
+++ utils/sanity.js
@@ -16,8 +16,9 @@ const config = {
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
   apiVersion: 'v1',
-  useCdn: true,
-  token: '',
+  // useCdn: false,
+  useCdn: process.env.NODE_ENV === 'production',
+  // token: '',
   // token: process.env.SANITY_API_TOKEN,
   // useCdn: process.env.NODE_ENV === 'production'
   /**
