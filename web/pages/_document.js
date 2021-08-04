b2155e0fa13b345fe6545dfcf9c7fb9017c8c78c
Brent Robbins <brent@variantstudios.com>
2021-08-04 05:11:28 -0600

    Updating the _document.js external js

--- web/pages/_document.js
+++ web/pages/_document.js
@@ -16,32 +16,28 @@ class MyDocument extends Document {
           src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
         />
         <script
+          async
+          src={`/iframeResizer.contentWindow.min.js`}
+        />
+        <script
           dangerouslySetInnerHTML={{
             __html: `
+            // Google Analytics
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
           gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
             page_path: window.location.pathname,
           });
+          // iFrameResizer
+          window.iFrameResizer = {
+            onReady: function() {
+              var myId = window.parentIFrame.getId()
+            }
+          }
         `,
           }}
           />
-
-          <Script
-          dangerouslySetInnerHTML={{
-            __html: `
-            window.iFrameResizer = {
-              onReady: function() {
-                var myId = window.parentIFrame.getId()
-              }
-            }
-            `,
-          }}
-          />
-          <Script
-          src='/iframeResizer.contentWindow.min.js'
-        />
         </Head>
         <body>
           <Main />
